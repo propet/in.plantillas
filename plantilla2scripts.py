436f7153625ffbaad9e50c527696390c67d50557
propet <luisdomingoaranda@gmail.com>
2016-04-18 21:11:42 +0200

    cambios en plantilla2scripts

--- plantilla2scripts.py
+++ plantilla2scripts.py
@@ -23,7 +23,7 @@ prop_step = 5       # Paso porcentual en la proporcion de MgO en la mezcla
 mu_step = 300	    # micras
 sigma_step = 50
 # Numero de diferentes radios para aproximar la distribucion gausiana con valores discretos
-n_sizes = 30.0
+n_sizes = 30.0 # Importante tomar este valor como un float
 radioMinimoConsiderado = 100
 
 ##########################################
