faf1226bef3681b8cb8afaf2ecd1bfbd906a5e9e
propet <luisdomingoaranda@gmail.com>
2016-04-29 14:18:40 +0200

    modulo de young corregido

---
+++ ejecuta.py
@@ -0,0 +1,13 @@
+import os
+import glob
+import math
+import numpy as np
+
+os.system('mpirun -np * liggghts < in1.MgO_proporcion_Rmin_radioMinimo_Rmax_radioMaximo')
+os.system('mpirun -np * liggghts < in2.MgO_proporcion_Rmin_radioMinimo_Rmax_radioMaximo')
+os.system('mpirun -np * liggghts < in3.MgO_proporcion_Rmin_radioMinimo_Rmax_radioMaximo')
+newest = max(glob.iglob('./post/dump*.pruebas'), key=os.path.getctime)
+os.system('mv %s ./dump1' % (newest))
+os.system('mpirun -np * liggghts < in4.MgO_proporcion_Rmin_radioMinimo_Rmax_radioMaximo')
+newest = max(glob.iglob('./post/dump*.pruebas'), key=os.path.getctime)
+os.system('mv %s ./dump2' % (newest))
