7a0b8419f184be53dde4ffbfdf522260c843a700
propet <luisdomingoaranda@gmail.com>
2016-04-30 21:53:31 +0200

    para cada porcentaje de MgO

--- bateriaSimulaciones.py
+++ bateriaSimulaciones.py
@@ -3,32 +3,40 @@ import glob
 import math
 import numpy as np
 
+'''
+	Bateria de simulaciones con distribucion en masa uniforme.
+	Ejecuta simulaciones en funcion de la proporcion de MgO, de la fuerza maxima aplicada y 
+	del radio minimo en la distribucion uniforme.
+'''
+
 # Poder manipular la carpeta sin que me vuelvan a pedir permiso
-os.system('sudo chmod -R 777 ./scripts')
 os.system('sudo mkdir dumps')
 os.system('sudo mkdir scripts')
 os.system('sudo mkdir post')	
+os.system('sudo chmod -R 777 ./*')
 
-MgO = '75.0'
-directorioProporcion = './scripts/input_MgO-' + MgO
-fuerzas = [1000, 100000, 10000000] # Newtons -> 1kN, 100kN, 10T
+MgO = ['70.0', '80.0', '90.0']
+for proporcion in MgO:
 
-for fuerza in fuerzas:
-	os.system("sed -e 's/FUERZA/%d/g' in.plantilla2.0 > in.plantilla2" % (fuerza))
-	os.system("sed -e 's/PROPMGO/%.0f/g' -e 's/FUERZA/%d/g' ejecuta0.py > ejecuta.py" % (float(MgO), fuerza))
-	os.system('python plantilla2scripts.py')
-	directorioMgO = os.listdir(directorioProporcion)
-	directorioMgO.sort()
+	directorioProporcion = './scripts/input_MgO-' + proporcion
+	fuerzas = [1000, 100000, 10000000] # Newtons -> 1kN, 100kN, 10T
 
-	for caso in directorioMgO:
-		if(caso.endswith('1300') and not(caso.startswith('Rmin_100'))):
-			os.chdir(directorioProporcion + '/' + caso)
-			os.system('python ejecuta.py')
-			# archivos dump para posible renderizado posterior
-			os.system('mv dump1 ../../../dumps/dump1%s-F_%sN' % (caso, str(fuerza)))
-			os.system('mv dump2 ../../../dumps/dump2%s-F_%sN' % (caso, str(fuerza)))
-			# Vuelve a directorio simulaciones
-			os.chdir('../../../')
+	for fuerza in fuerzas:
+		os.system("sed -e 's/FUERZA/%d/g' in.plantilla2.0 > in.plantilla2" % (fuerza))
+		os.system("sed -e 's/PROPMGO/%.0f/g' -e 's/FUERZA/%d/g' ejecuta0.py > ejecuta.py" % (float(proporcion), fuerza))
+		os.system('python plantilla2scripts.py')
+		directorioMgO = os.listdir(directorioProporcion)
+		directorioMgO.sort()
+
+		for caso in directorioMgO:
+			if(caso.endswith('1300') and not(caso.startswith('Rmin_100'))):
+				os.chdir(directorioProporcion + '/' + caso)
+				os.system('python ejecuta.py')
+				# archivos dump para posible renderizado posterior
+				os.system('mv dump1 ../../../dumps/dump1MgO_%s-%s-F_%sN' % (proporcion, caso, str(fuerza)))
+				os.system('mv dump2 ../../../dumps/dump2MgO_%s-%s-F_%sN' % (proporcion, caso, str(fuerza)))
+				# Vuelve a directorio simulaciones
+				os.chdir('../../../')
 
 
 print 'Tareas completadas! :D'
