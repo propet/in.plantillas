4ef18d1cfd1dfe81bc505517adc5a0631731d802
propet <luisdomingoaranda@gmail.com>
2016-07-12 21:42:37 +0200

    primer commit para 802, faltan in.2, in.3, in.4

---
+++ ejecuta.py
@@ -0,0 +1,13 @@
+import os
+import glob
+import math
+import numpy as np
+
+os.system('mpirun -np 4 liggghts < in.plantilla1')
+os.system('mpirun -np 4 liggghts < in.plantilla2')
+os.system('mpirun -np 4 liggghts < in.plantilla3')
+newest = max(glob.iglob('./post/dump*.pruebas'), key=os.path.getctime) # particulas antes de retirar paredes
+os.system('mv %s ./dump1' % (newest))
+os.system('mpirun -np 4 liggghts < in.plantilla4')
+newest = max(glob.iglob('./post/dump*.pruebas'), key=os.path.getctime) # particulas en reposos sin paredes
+os.system('mv %s ./dump2' % (newest))
