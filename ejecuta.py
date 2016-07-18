8c83ad677565243ef847582c38550a63837655ac
propet <luisdomingoaranda@gmail.com>
2016-07-18 13:36:51 +0200

    solo compresion

--- ejecuta.py
+++ ejecuta.py
@@ -5,9 +5,9 @@ import numpy as np
 
 os.system('mpirun -np 4 liggghts < in.plantilla1')
 #os.system('mpirun -np 4 liggghts < in.plantilla2')
-os.system('mpirun -np 4 liggghts < in.plantilla3')
+#os.system('mpirun -np 4 liggghts < in.plantilla3')
 newest = max(glob.iglob('./post/dump*.pruebas'), key=os.path.getctime) # particulas antes de retirar paredes
 os.system('mv %s ./dump1' % (newest))
-os.system('mpirun -np 4 liggghts < in.plantilla4')
-newest = max(glob.iglob('./post/dump*.pruebas'), key=os.path.getctime) # particulas en reposos sin paredes
-os.system('mv %s ./dump2' % (newest))
+#os.system('mpirun -np 4 liggghts < in.plantilla4')
+#newest = max(glob.iglob('./post/dump*.pruebas'), key=os.path.getctime) # particulas en reposos sin paredes
+#os.system('mv %s ./dump2' % (newest))
