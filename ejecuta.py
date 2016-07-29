dff52996e5b77812d766f1a4eb6a20dba5ca1d87
propet <luisdomingoaranda@gmail.com>
2016-07-29 13:30:56 +0200

    calcula la fuerza al retirar la prensa tambien

--- ejecuta.py
+++ ejecuta.py
@@ -5,7 +5,7 @@ import numpy as np
 
 os.system('mpirun -np 4 liggghts < in.plantilla1')
 #os.system('mpirun -np 4 liggghts < in.plantilla2')
-#os.system('mpirun -np 4 liggghts < in.plantilla3')
+os.system('mpirun -np 4 liggghts < in.plantilla3')
 newest = max(glob.iglob('./post/dump*.pruebas'), key=os.path.getctime) # particulas antes de retirar paredes
 os.system('mv %s ./dump1' % (newest))
 #os.system('mpirun -np 4 liggghts < in.plantilla4')
